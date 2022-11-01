7f4aa567ec61e9f03d4769a05faeee25982c7f9a
mustafa <mustafa.bhm3@gmail.com>
2022-11-01 16:50:03 -0600

    solving repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (s, num) {
+  let result = "";
+  if (num === 0) {
+    result = "";
+  } else if (num === 1) {
+    result = s;
+  } else if (num < 0) {
+    result = "ERROR";
+  } else {
+    for (i = 1; i <= num; i++) {
+      result += s;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
