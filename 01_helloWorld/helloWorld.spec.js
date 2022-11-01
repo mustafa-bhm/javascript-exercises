3f599bc029450eca405401064d72397fe2e9cd1a
mustafa <mustafa.bhm3@gmail.com>
2022-11-01 16:34:09 -0600

    setup

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,8 @@
-const helloWorld = require('./helloWorld');
+const helloWorld = require("./helloWorld");
 
-describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
-    expect(helloWorld()).toEqual('Hello, World!');
+describe("Hello World", function () {
+  test('says "helloWorld, World!" ', function () {
+    expect(helloWorld()).toEqual("Hello, World!");
   });
 });
+//
