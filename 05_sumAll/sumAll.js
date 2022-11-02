9787a6704f2dfc82d700201bd3902d5a4bb5aabf
mustafa <mustafa.bhm3@gmail.com>
2022-11-02 10:53:39 -0600

    solving sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let result = 0;
+  if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+  for (let i = start; i < end + 1; i++) {
+    result += i;
+  }
 
+  return result;
 };
 
+console.log(sumAll(4, 1)); // => 10
 // Do not edit below this line
 module.exports = sumAll;
