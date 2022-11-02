1240bb74bfc0b1155368c630a202cd8f6b4fe628
mustafa <mustafa.bhm3@gmail.com>
2022-11-02 12:17:41 -0600

    solving temo conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function (t) {
+  let result = (t - 32) * 0.5556;
+  return Math.round(result * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (t) {
+  return Math.round((t * 1.8 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
