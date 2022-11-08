11eaba28b1ebec0094db1a736135f18b58007043
mustafa <mustafa.bhm3@gmail.com>
2022-11-08 13:34:49 -0700

    solving palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  let string = s.toLowerCase().replace(/[^a-z]/g, "");
+  let reversedStr = string.split("").reverse().join("");
+  if (string === reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
