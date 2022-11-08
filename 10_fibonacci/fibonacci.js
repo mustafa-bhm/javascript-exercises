1cd1ec79ffab318c75db340b99ec86024e1ddffa
mustafa <mustafa.bhm3@gmail.com>
2022-11-08 14:15:20 -0700

    solving fibonacci fun

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+  // to create fibonacci sequence
 
+  let n1 = 0;
+  let n2 = 1;
+  let nextNum;
+  let res = [];
+  for (let i = 1; i < 300; i++) {
+    nextNum = n1 + n2;
+    n1 = n2;
+    n2 = nextNum;
+    res.push(n1);
+  }
+  // to return the approprite num
+  if (a < 0) {
+    return "OOPS";
+  } else {
+    let selectNum = res.splice(a - 1, 1);
+    return Number(selectNum);
+  }
 };
 
 // Do not edit below this line
